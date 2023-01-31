93e8326111376185faa4c2c50630c7aec24a74de
UtsuboHiroaki <hutsubo@gmail.com>
2023-01-31 21:21:42 +0900

    settings_local.py作成 20230131 V1

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wm-eoqvims^0w$(4-i0-^%gngp8n@j4vckh@=0e%_2nu&bfc_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
